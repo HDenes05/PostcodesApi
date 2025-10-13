33b24df68ed7f09075b3c20e003741b40f8a37bb
nzsolt0601 <asd@asd>
2025-10-13 10:53:35 +0200

    laravel-zip-api conrollers

--- app/Models/County.php
+++ app/Models/County.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class County extends Model
+
+class  County extends Model
 {
     public $timestamps = false;
     protected $fillable = ['name'];
